f59f79b47498f086059159d3ba2e423bbbef6303
parthureddy <parthuredy3@gmail.com>
2025-02-08 16:32:44 -0600

    updated by parthu

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -6,23 +6,43 @@ resource "aws_instance" "web" {
     Name    = "Terraform-demo"
     Purpose = "practicing terraform"
   }
-  provisioner "local-exce" {
-  command = "echo ${self.private_ip} > inventory"
-}
-}
 
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} > inventory"
+  }
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+  }
+
+    provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install epel-release -y",
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+}
+    provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo systemctl stop nginx",
+    ]
+}
+}
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   ingress {
     from_port   = 22
@@ -31,6 +51,12 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
 
   }
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
 
   tags = {
     Name = "allow_tls"
---
+++ provisioners/variables.tf
@@ -0,0 +1,9 @@
+variable "user_id" {
+    type = string
+    default = "ec2-user"
+}
+
+variable "password" {
+    type = string
+    default = "DevOps321"
+}
